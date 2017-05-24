f7203bca70c15d72943abe9d10cfcb4058eaf5a5
zzx <piofcircle@gmail.com>
2017-05-24 22:46:01 +0800

    Initial commit

---
+++ src/main/java/com/zzx/sample/Sample.java
@@ -0,0 +1,13 @@
+package com.zzx.sample;
+
+import com.zzx.math.BasicMath;
+
+/**
+ * Created by Administrator on 2017/5/24 0024.
+ */
+public class Sample {
+    public static void main(String[] args) {
+        BasicMath basicMath = new BasicMath();
+        System.out.println(basicMath.add(1, 3));
+    }
+}
